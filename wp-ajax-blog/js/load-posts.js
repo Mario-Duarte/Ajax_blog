9605f820f7c26542849d3352eea7e3f85ab8580f
Mario <Mario-Duarte@users.noreply.github.com>
2016-07-01 16:56:20 +0100

    Update load-posts.js

--- wp-ajax-blog/js/load-posts.js
+++ wp-ajax-blog/js/load-posts.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
 
-  if ( $('.blog').length > 0 ) {
+  if ( $('.blog,.category,.archive').length > 0 ){
 
     $('body').on('click', '.js-load-more-posts', function(e){
       e.preventDefault();
@@ -9,19 +9,48 @@ jQuery(document).ready(function($){
       $(this).closest('.js-load-more-posts').remove();
       loading.appendTo('.posts-wrapper');
 
+      var button = $(this);
+
       $.ajax({
         url: nextPostsSRC,
         method: "GET",
         success: function(data){
-            dataObject = $(data);
-            $('.loading-more-posts').remove();
-            var newPosts = $('.posts-wrapper',dataObject).contents();
-            $('.posts-wrapper').append( newPosts );
+          populateDiv(data);
+        },
+        error: function(){
+
+          var url = nextPostsSRC.replace(/\/$/, "");
+          url = url.split('/');
+          var page = url[url.length-1];
+          var pageOrigin = url.slice(0, (url.length)-2);
+          pageOrigin = pageOrigin.join('/');
+          $.ajax({
+            url: pageOrigin,
+            method: "GET",
+            data: {pagedNum: page},
+            success: function(data){
+                populateDiv(data);
+            },
+            dataType: "html"
+          });
+          url = page = pageOrigin = null;
+
         },
         dataType: "html"
       });
 
     });
+    function ajaxRequest(url,data){
+
+
+    }
+
+    function populateDiv(data){
+      dataObject = $(data);
+        $('.loading-more-posts').remove();
+        var newPosts = $('.posts-wrapper',dataObject).contents();
+        $('.posts-wrapper').append( newPosts );
+    }
 
   } //endif
 
